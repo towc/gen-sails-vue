a161551510c8f1e2ec5813023a53e5dfd196fc90
Matei Copot <matei@copot.eu>
2017-09-06 22:11:17 +0200

    prettified comments

--- create-vue.sh
+++ create-vue.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
-echo "################################################" &&
-echo "# generating frontend with custom vue template #" &&
-echo "################################################" &&
+echo "" &&
+echo "# 1" &&
+echo "# generating frontend with custom vue template" &&
+echo "" &&
 vue init towc/vue-template-webpack-sails frontend &&
+echo "# SUCCESS 1" &&
 
-echo "###############################" &&
-echo "# entering frontend directory #" &&
-echo "###############################" &&
+echo "" &&
+echo "# 2" &&
+echo "# entering frontend directory" &&
+echo "" &&
 cd frontend &&
+echo "# SUCCESS 2" &&
 
-echo "###########################" &&
+echo "" &&
+echo "# 3" &&
 echo "# installing npm packages #" &&
-echo "###########################" &&
+echo "" &&
 npm i &&
+echo "# SUCCESS 3" &&
 
-echo "#########################" &&
-echo "# running build scripts # 4/4" &&
-echo "#########################" &&
+echo "" &&
+echo "# 4 (END)" &&
+echo "# running build scripts" &&
+echo "" &&
 npm run build
--- create.sh
+++ create.sh
@@ -1,41 +1,60 @@
 #!/bin/bash
 
-echo "#################################" &&
-echo "# Creating new terminal for Vue # 1/8" &&
-echo "#################################" &&
+echo ""
+echo "# 1" &&
+echo "# Creating new terminal for Vue" &&
+echo "" &&
 (x-terminal-emulator -e ./create-vue.sh &);
+echo "# SUCCESS 1" &&
 
-echo "#################" &&
-echo "# removing .git # 2/8" &&
-echo "#################" &&
+echo "" &&
+echo "# 2" &&
+echo "# removing .git" &&
+echo "" &&
 rm -rf .git &&
+echo "# SUCCESS 2" &&
 
-echo "###########################################" &&
-echo "# removing LICENSE, README and .gitignore # 3/8" &&
-echo "###########################################" &&
+echo "" &&
+echo "# 3" &&
+echo "# removing LICENSE, README and .gitignore" &&
+echo "" &&
 rm LICENSE README.md .gitignore &&
+echo "# SUCCESS 3" &&
 
-echo "########################################" &&
-echo "# generating backend folder with sails # 4/8" &&
-echo "########################################" &&
+echo "" &&
+echo "# 4" &&
+echo "# generating backend folder with sails" &&
+echo "" &&
 sails new backend &&
+echo "# SUCCESS 4" &&
 
-echo "##############################" &&
-echo "# entering backend directory # 5/8" &&
-echo "##############################" &&
+echo "" &&
+echo "# 5" &&
+echo "# entering backend directory" &&
+echo "" &&
 cd backend &&
+echo "# SUCCESS 5" &&
 
-echo "########################################" &&
-echo "# generating vue controller with sails # 6/8" &&
-echo "########################################" &&
+echo "" &&
+echo "# 6" &&
+echo "# generating vue controller with sails" &&
+echo "" &&
 sails generate controller vue &&
+echo "# SUCCESS 6" &&
 
-echo "#################################" &&
-echo "# replacing vue controller file # 7/8" &&
-echo "#################################" &&
+echo "" &&
+echo "# 7" &&
+echo "# replacing vue controller file" &&
+echo "" &&
 mv ../vue-controller-serve.js api/controllers/VueController.js &&
+echo "# SUCCESS 7" &&
 
-echo "############################################" &&
-echo "# removing shell scripts for final cleanup # 8/8" &&
-echo "############################################" &&
+echo "" &&
+echo "# 8" &&
+echo "# removing shell scripts for final cleanup" &&
 rm ../create.sh ../create-vue.sh
+echo "# SUCCESS 8" &&
+
+echo "# END" &&
+echo "" &&
+echo "# feel free to \`cd backend\` and \`sails lift\` in any way you want!"
