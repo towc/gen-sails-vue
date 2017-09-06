9beff43018a037f0e3dd70b50e6694fe40af4c3d
Matei Copot <matei@copot.eu>
2017-09-06 21:57:40 +0200

    added comments for easier debugging

--- create-vue.sh
+++ create-vue.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
+echo "################################################" &&
+echo "# generating frontend with custom vue template #" &&
+echo "################################################" &&
 vue init towc/vue-template-webpack-sails frontend &&
+
+echo "###############################" &&
+echo "# entering frontend directory #" &&
+echo "###############################" &&
 cd frontend &&
+
+echo "###########################" &&
+echo "# installing npm packages #" &&
+echo "###########################" &&
 npm i &&
+
+echo "#########################" &&
+echo "# running build scripts # 4/4" &&
+echo "#########################" &&
 npm run build
--- create.sh
+++ create.sh
@@ -1,10 +1,41 @@
 #!/bin/bash
 
+echo "#################################" &&
+echo "# Creating new terminal for Vue # 1/8" &&
+echo "#################################" &&
 (x-terminal-emulator -e ./create-vue.sh &);
+
+echo "#################" &&
+echo "# removing .git # 2/8" &&
+echo "#################" &&
 rm -rf .git &&
+
+echo "###########################################" &&
+echo "# removing LICENSE, README and .gitignore # 3/8" &&
+echo "###########################################" &&
 rm LICENSE README.md .gitignore &&
+
+echo "########################################" &&
+echo "# generating backend folder with sails # 4/8" &&
+echo "########################################" &&
 sails new backend &&
+
+echo "##############################" &&
+echo "# entering backend directory # 5/8" &&
+echo "##############################" &&
 cd backend &&
+
+echo "########################################" &&
+echo "# generating vue controller with sails # 6/8" &&
+echo "########################################" &&
 sails generate controller vue &&
+
+echo "#################################" &&
+echo "# replacing vue controller file # 7/8" &&
+echo "#################################" &&
 mv ../vue-controller-serve.js api/controllers/VueController.js &&
+
+echo "############################################" &&
+echo "# removing shell scripts for final cleanup # 8/8" &&
+echo "############################################" &&
 rm create.sh create-vue.sh
